275e913d9ad05180b0a4ae7d42bfcdab63bef816
kaikecod <kaikemioranza@outlook.com>
2025-11-25 21:44:59 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -3,16 +3,27 @@ import java.util.Enumeration;
 public class HtmlStatement extends Statement {
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+        String result = headerString(aCustomer);
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            result += each.getMovie().getTitle() + ": " +
-                    String.valueOf(each.getCharge()) + "<BR>\n";
+            result += eachRentalString(each);
         }
-        result += "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" +
+        result += footerString(aCustomer);
+        return result;
+    }
+    protected String headerString(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
+
+    protected String eachRentalString(Rental aRental) {
+        return aRental.getMovie().getTitle() + ": " +
+                String.valueOf(aRental.getCharge()) + "<BR>\n";
+    }
+
+    protected String footerString(Customer aCustomer) {
+        return "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" +
+                "On this rental you earned <EM>" +
                 String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
                 "</EM> frequent renter points<P>";
-        return result;
     }
 }
